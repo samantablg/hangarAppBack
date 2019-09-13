edc8e796d56c39bd5bc47f96cfc063e631d7f709
Samanta Belen Lara <samanta.lara@plexus.es>
2019-09-13 13:50:41 +0100

    manage order

--- commerce/src/main/java/com/myApp/order/service/OrderServiceImpl.java
+++ commerce/src/main/java/com/myApp/order/service/OrderServiceImpl.java
@@ -29,21 +29,11 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order saveOrder(Order order) {
-        try { //TODO cambiar relación en las entidades para que se guarde product_order solo y mejorar código
-            List<Product_Order> products_order = order.getProducts_orders().stream().map(
-                    product_order -> {
-                        Product_Hangar product_hangar = updateStockAfterOrder(product_order);
-                        return product_orderService.save(product_order);
-                    }
-            ).collect(Collectors.toList());
-            order.setProducts_orders(products_order);
-            if (order.getTotal_price() == this.calculatePriceOfOrder(order))
-                return orderDao.saveOrder(order);
-            throw new ApplicationException(ApplicationExceptionCause.PRICE_CONFLICT);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+        List<Product_Order> products_order = this.saveProduct_Order(order);
+        order.setProducts_orders(products_order);
+        if (order.getTotal_price() == this.calculatePriceOfOrder(order) && order.getTotal_products() == this.getTotalProductsOrder(order))
+            return orderDao.saveOrder(order);
+        throw new ApplicationException(ApplicationExceptionCause.PRICE_CONFLICT); //change conflict
     }
 
     @Override
@@ -73,8 +63,6 @@ public class OrderServiceImpl implements OrderService {
         ).sum();
     }
 
-
-
     private List<Product_Order> saveProduct_Order(Order order) {
         return order.getProducts_orders().stream().map(
                 product_order -> {
@@ -87,4 +75,11 @@ public class OrderServiceImpl implements OrderService {
     private Product_Hangar updateStockAfterOrder(Product_Order product_order) {
         return product_hangarService.updateAmountAfterOrder(product_order.getProduct_id(), product_order.getHangar_id(), product_order.getQuantity());
     }
+
+    private long getTotalProductsOrder(Order order) {
+        return order.getProducts_orders().stream().mapToLong(
+                product_order -> product_order.getQuantity()
+        ).sum();
+    }
+
 }
